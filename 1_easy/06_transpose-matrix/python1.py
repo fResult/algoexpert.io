9a54248c8480932b8f4809fb1578d7ac58087c59
fResult <styxmaz@gmail.com>
2024-10-21 23:51:36 +0700

    add type for python solution

--- 1_easy/06_transpose-matrix/python1.py
+++ 1_easy/06_transpose-matrix/python1.py
@@ -1,8 +1,8 @@
 # O(w x h) time | O(w x h) space
-def transpose_matrix(matrix):
-    transposed_matrix = []
+def transpose_matrix(matrix: list[list[int]]) -> list[list[int]]:
+    transposed_matrix: list[list[int]] = []
     for col in range(len(matrix[0])):
-        new_row = []
+        new_row: list[int] = []
         for row in range(len(matrix)):
             new_row.append(matrix[row][col])
         transposed_matrix.append(new_row)
