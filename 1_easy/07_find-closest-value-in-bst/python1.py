67e54c8c4d28e63c0fc9971b389d0d2056b07f63
fResult <styxmaz@gmail.com>
2024-10-24 09:09:38 +0700

    update function signature for `Find closest value in BST` python solution

--- 1_easy/07_find-closest-value-in-bst/python1.py
+++ 1_easy/07_find-closest-value-in-bst/python1.py
@@ -5,12 +5,12 @@ class BST:
         self.right: BST | None = None
 
 
-def findClosestValueInBst(tree: BST | None, target: int):
+def findClosestValueInBst(tree: BST | None, target: int) -> int:
     return findClosestValueInBstHelper(tree, target, float("inf"))
 
-def findClosestValueInBstHelper(tree: BST | None, target: int, closest: float):
+def findClosestValueInBstHelper(tree: BST | None, target: int, closest: float) -> int:
     if tree is None:
-        return closest
+        return int(closest)
 
     if abs(target - closest) > abs(target - tree.value):
         closest = tree.value
@@ -20,7 +20,7 @@ def findClosestValueInBstHelper(tree: BST | None, target: int, closest: float):
     elif target > tree.value:
         return findClosestValueInBstHelper(tree.right, target, closest)
     else:
-        return closest
+        return int(closest)
 
 # root: BST = BST(12)
 # root.left = BST(1)
